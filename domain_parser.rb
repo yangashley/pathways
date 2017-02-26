d648cdf3800fbd1db8aa3c0373f44c29f3270a41
Ashley Yang <ashley.minghui@gmail.com>
2017-02-26 15:18:58 -0600

    Add parse method to DomainParser module

--- domain_parser.rb
+++ domain_parser.rb
@@ -1,2 +1,11 @@
+require 'csv'
+
 module DomainParser
+	def self.parse(filename)
+		domain_order_hash = {}
+		CSV.foreach(filename) do |row|
+			domain_order_hash[row[0]] = row[1..-1]
+		end
+		domain_order_hash
+	end
 end
--- spec/domain_parser_spec.rb
+++ spec/domain_parser_spec.rb
@@ -2,9 +2,9 @@ require_relative '../domain_parser'
   		  
 describe DomainParser do
 
-  let(:domain_data) { DomainParser.parse('domain_order.csv') }
+  let(:domain_data) { DomainParser.parse('data/domain_order.csv') }
 
-  it 'should return a hash of domains' do
+  it 'should return a hash with the grade as a key and an array of domains as the value' do
     expect(domain_data).to be_a Hash
   end
 end		  
