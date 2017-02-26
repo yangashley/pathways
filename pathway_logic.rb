0b0adf4c1467cc787c0afdbd97b026e23da5be22
Ashley Yang <ashley.minghui@gmail.com>
2017-02-26 17:47:14 -0600

    Write convert_letter_k_to_zero method for PathwayLogic module, tests pass

--- pathway_logic.rb
+++ pathway_logic.rb
@@ -1,3 +1,11 @@
 module PathwayLogic
-
+	def self.convert_letter_k_to_zero(test_hash)
+		test_hash.each do |key, value|
+			if value == "K"
+				test_hash[key] = 0
+			end
+		end
+		test_hash
+	end
 end
+  
