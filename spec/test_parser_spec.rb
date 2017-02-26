cefa9208b56bc8e9a2d31e9c50fc0f4484919644
Ashley Yang <ashley.minghui@gmail.com>
2017-02-26 15:51:05 -0600

    Write parse method for TestParser module, pass tests

--- spec/test_parser_spec.rb
+++ spec/test_parser_spec.rb
@@ -5,18 +5,22 @@ describe TestParser do
   before(:all) do
     @test_data = TestParser.parse('data/student_tests.csv')
 	end
+
   it 'should return an array of hashes' do
     expect(@test_data).to be_an Array
+    p @test_data[0]
+
   end
 
   context "hashes in the array" do
+  	let(:test_hash) { @test_data[0] }
+
     it 'have a student name for the value of the first key-value pair' do 
-    	test_hash = @test_data[0]
-    	expect(test_hash[0]).to eq "Albin Stanton"
+    	expect(test_hash["Student Name"]).to eq "Albin Stanton"
   	end
 
 	  it 'have a grade level for the value of any key-value pair excluding the first one' do 
-	  	expect(test_hash[1]). to eq "2"
+	  	expect(test_hash["RF"]). to eq "2"
 	  end
   end
 
