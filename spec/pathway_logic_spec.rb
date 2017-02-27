83e9bd94c3a6f7047192b206e25463a50476aea9
Ashley Yang <ashley.minghui@gmail.com>
2017-02-27 12:49:06 -0600

    Add test for #lowest_grade_domain_order for PathwayLogic

--- spec/pathway_logic_spec.rb
+++ spec/pathway_logic_spec.rb
@@ -3,6 +3,7 @@ require_relative '../pathway_logic'
 describe 'PathwayLogic' do 
   before(:all) do
     @test_data = {"Student Name"=>"Albin Stanton", "RF"=>"2", "RL"=>"3", "RI"=>"K", "L"=>"3"} 
+    @parsed_domains = {"K"=>["RF", "RL", "RI"], "1"=>["RF", "RL", "RI"], "2"=>["RF", "RI", "RL", "L"], "3"=>["RF", "RL", "RI", "L"], "4"=>["RI", "RL", "L"], "5"=>["RI", "RL", "L"], "6"=>["RI", "RL"]}
 	end
 
 	describe '#convert_letter_k_to_zero' do 
@@ -26,6 +27,14 @@ describe 'PathwayLogic' do
 	    expect(PathwayLogic.find_lowest_domains(@test_data)).to be_a Hash
  		end		
  	end
+
+ 	describe '#lowest_grade_domain_order' do 
+ 		it 'returns an array of domains in correct learning order for the lowest grade level' do 
+	    expect(PathwayLogic.lowest_grade_domain_order(@test_data, @parsed_domains)).to be_an Array
+ 		end		
+ 	end 	
+
+ 	
 
 	# describe '#lowest_domain' do
 
