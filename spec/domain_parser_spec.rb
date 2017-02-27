7a86741b6078bb48d0c061db8ab828bebc7416ad
Ashley Yang <ashley.minghui@gmail.com>
2017-02-27 12:26:49 -0600

    Delete domain_parser test for converting K to 0

--- spec/domain_parser_spec.rb
+++ spec/domain_parser_spec.rb
@@ -7,8 +7,4 @@ describe DomainParser do
   it 'should return a hash with the grade as a key and an array of domains as the value' do
     expect(domain_data).to be_a Hash
   end
-
-  it 'should represent K for kindergarten as 0' do 
-  	expect(domain_data).to include("0"=>["RF", "RL", "RI"])
-  end
 end		  
