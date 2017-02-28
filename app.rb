e40ac67f49170247382590192500e0d4c64765d2
Ashley Yang <ashley.minghui@gmail.com>
2017-02-27 18:35:48 -0600

    Add test for #add_first_domain_to_pathway in app.rb

--- app.rb
+++ app.rb
@@ -8,18 +8,15 @@ require_relative 'pathway'
 test_data = TestParser.parse('data/student_tests.csv')
 domain_data = DomainParser.parse('data/domain_order.csv') 
 
-# test_data.each do |student_hash|
-# 	p student_hash["Student Name"]
-# end 
-
-# test_data = [{"Student Name"=>"Albin Stanton", "RF"=>"2", "RL"=>"3", "RI"=>"K", "L"=>"3"}, {"Student Name"=>"Erik Purdy", "RF"=>"3", "RL"=>"1", "RI"=>"1", "L"=>"1"}]...
+pathways = []
+test_data.each do |student_hash|
+	pathways << Pathway.new(student_hash["Student Name"])
+end 
 
 # convert all 'K's to 0
 converted_data = test_data.each do |student_data_hash|
 	PathwayLogic.convert_letter_k_to_zero(student_data_hash)
 end
-
-p converted_data
 
 # find the lowest domains for each student, returns an array of hashes with no names
 lowest_domains = []
@@ -31,6 +28,7 @@ first_domains = []
 lowest_domains.each do |student_data_hash|
 	first_domains << PathwayLogic.starting_domain(student_data_hash, domain_data)
 end
+
 
 
 
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -1,4 +1,17 @@
 require_relative '../app'
 
 describe 'App' do 
+	describe '#add_first_domain_to_pathway' do 
+		let(:pathways) {[Pathway.new("Ron Weaseley"), Pathway.new("Harry Potter")]}
+		let(:first_domains) {["2.RI", "1.RF"]}
+
+		it 'returns an array of Pathway objects that each have a student name and first domain' do
+			expect(add_first_domain_to_pathway(pathways, first_domains)).to be_an Array
+		end
+
+		it 'returns an array of Pathway objects that each have a student name and first domain' do
+			updated_pathways = add_first_domain_to_pathway(pathways, first_domains)
+			expect(updated_pathways.first.goal_domains).to eq "2.RI"
+		end
+	end
 end
