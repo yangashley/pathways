474a33eec1cf072aa0b3435d9ffebfcb149e27ce
Ashley Yang <ashley.minghui@gmail.com>
2017-02-28 16:40:23 -0600

    Change .each to .map for methods

--- app.rb
+++ app.rb
@@ -7,24 +7,24 @@ test_data = TestParser.parse('data/student_tests.csv')
 domain_data = DomainParser.parse('data/domain_order.csv') 
 
 # wrap these sections in their own methods
+# lines 11-18 can go in parsing modules
 pathways = []
 test_data.each do |student_hash|
 	pathways << Pathway.new(student_hash["Student Name"])
 end 
 
-converted_data = test_data.each do |student_data_hash|
+converted_data = test_data.map do |student_data_hash|
 	PathwayLogic.convert_letter_k_to_zero(student_data_hash)
 end
 
 # find the lowest domains for each student, returns an array of hashes with no names
-lowest_domains = []
-converted_data.each do |student_data_hash|
-	lowest_domains << PathwayLogic.find_lowest_domains(student_data_hash)
+# .each to .map
+lowest_domains = converted_data.map do |student_data_hash|
+	PathwayLogic.find_lowest_domains(student_data_hash)
 end
 
-first_domains = []
-lowest_domains.each do |student_data_hash|
-	first_domains << PathwayLogic.starting_domain(student_data_hash, domain_data)
+first_domains = lowest_domains.map do |student_data_hash|
+	PathwayLogic.starting_domain(student_data_hash, domain_data)
 end
 
 def add_first_domain_to_pathway(array_of_pathways, array_of_first_domains)
