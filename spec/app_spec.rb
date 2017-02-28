a69e1aa22fbfff4fe661ca16b88dcf72abde2840
Ashley Yang <ashley.minghui@gmail.com>
2017-02-27 18:42:37 -0600

    Change the description for the second app test

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -5,11 +5,11 @@ describe 'App' do
 		let(:pathways) {[Pathway.new("Ron Weaseley"), Pathway.new("Harry Potter")]}
 		let(:first_domains) {["2.RI", "1.RF"]}
 
-		it 'returns an array of Pathway objects that each have a student name and first domain' do
+		it 'returns an array of Pathway objects' do
 			expect(add_first_domain_to_pathway(pathways, first_domains)).to be_an Array
 		end
 
-		it 'returns an array of Pathway objects that each have a student name and first domain' do
+		it 'adds the first domain to the goal domains for a pathway' do
 			updated_pathways = add_first_domain_to_pathway(pathways, first_domains)
 			expect(updated_pathways.first.goal_domains.first).to eq "2.RI"
 		end
