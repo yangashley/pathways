2cb61e71dd2643b6c30e2b91dcc4305e8b521590
Ashley Yang <ashley.minghui@gmail.com>
2017-03-01 12:56:44 -0600

    Write #parse_tests, pass tests. May need to get rid of #parse for TestParser

--- spec/test_parser_spec.rb
+++ spec/test_parser_spec.rb
@@ -1,25 +1,46 @@
 require_relative '../test_parser'
 
 describe TestParser do
+  describe '#parse' do
 
   before(:all) do
     @test_data = TestParser.parse('data/student_tests.csv')
 	end
+    it 'should return an array of hashes' do
+      expect(@test_data).to be_an Array
+    end
 
-  it 'should return an array of hashes' do
-    expect(@test_data).to be_an Array
+    context "hashes in the array" do
+    	let(:test_hash) { @test_data[0] }
+
+      it 'have a student name for the value of the first key-value pair' do 
+      	expect(test_hash["Student Name"]).to eq "Albin Stanton"
+    	end
+
+  	  it 'have a grade level for the value of any key-value pair excluding the first one' do 
+  	  	expect(test_hash["RF"]).to eq "2"
+  	  end
+    end
   end
 
-  context "hashes in the array" do
-  	let(:test_hash) { @test_data[0] }
+  describe '#parse_tests' do 
+    let(:student_data) { TestParser.parse_tests('data/student_tests.csv') }
 
-    it 'have a student name for the value of the first key-value pair' do 
-    	expect(test_hash["Student Name"]).to eq "Albin Stanton"
-  	end
+    it 'returns a hash' do 
+      expect(student_data).to be_a Hash
+    end
 
-	  it 'have a grade level for the value of any key-value pair excluding the first one' do 
-	  	expect(test_hash["RF"]).to eq "2"
-	  end
+    it 'keys of the hash are student names' do 
+      expect(student_data.keys[0]).to eq "Albin Stanton"
+    end 
+
+    it 'values of the hash are arrays representing the grade level and domain students scored in' do 
+      expect(student_data.values[0]).to eq ["2.RF", "3.RL", "K.RI", "3.L"]
+    end
+
+    it 'does not include headers from original CSV' do 
+      expect(student_data).not_to include "Student Name"
+    end 
   end
 
 end		  
--- test_parser.rb
+++ test_parser.rb
@@ -8,5 +8,20 @@ module TestParser
 		end
 		test_data_array
 	end
-end
 
+	def self.parse_tests(filename)
+		data_without_headers = CSV.read(filename)
+		all_headers = data_without_headers.shift
+		test_domains = all_headers[1..-1]
+
+		student_data = {}
+		data_without_headers.map do |student|
+			student_name = student.shift	
+			student_domains = student.map.with_index do |grade, index| 
+				"#{grade}.#{test_domains[index]}"
+			end
+			student_data.store(student_name, student_domains) 
+		end
+		student_data
+	end
+end
