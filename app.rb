c898a7c518327abc460bed779fb114fe733491b2
Ashley Yang <ashley.minghui@gmail.com>
2017-03-02 13:45:58 -0600

    Change variable names to be more descriptive in #build_pathway

--- app.rb
+++ app.rb
@@ -4,9 +4,6 @@ require_relative 'pathway_logic'
 require_relative 'pathway'
 require_relative 'pathway_writer'
 
-test_data_hash = TestParser.parse_tests('data/student_tests.csv')
-all_domains_ordered = DomainParser.parse_domains('data/domain_order.csv') 
-
 def generate_all_possible_grades(all_domains_ordered, domain)
 	domain = domain.split(".")[-1]
 	DomainParser.all_grades_in_domain(all_domains_ordered, domain)
@@ -18,15 +15,15 @@ def generate_all_domains(all_domains_ordered, domain)
 end
 
 def build_pathway(student_data_hash, all_domains_ordered)
-	final_pathway = Array.new
+	final_pathways = Array.new
 	student_data_hash.each do |student, test_scores|
-		entire_pathway = Array.new
+		entire_student_pathway = Array.new
 		test_scores.each do |domain|
-			entire_pathway << generate_all_domains(all_domains_ordered, domain)
+			entire_student_pathway << generate_all_domains(all_domains_ordered, domain)
 		end
-		final_pathway << PathwayLogic.produce_pathway(all_domains_ordered, entire_pathway.flatten.compact)
+		final_pathways << PathwayLogic.produce_pathway(all_domains_ordered, entire_student_pathway.flatten.compact)
 	end
-	final_pathway
+	final_pathways
 end
 
 def create_pathways(student_data_hash, all_final_pathways)
@@ -36,6 +33,9 @@ def create_pathways(student_data_hash, all_final_pathways)
 	end
 	all_pathways
 end
+
+test_data_hash = TestParser.parse_tests('data/student_tests.csv')
+all_domains_ordered = DomainParser.parse_domains('data/domain_order.csv')
 
 generated_pathways = build_pathway(test_data_hash, all_domains_ordered)
 pathways = create_pathways(test_data_hash, generated_pathways)
