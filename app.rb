6fdcc24dc0d1de601869d5b82699292d780c9bf9
Ashley Yang <ashley.minghui@gmail.com>
2017-03-02 15:55:43 -0600

    Change parameter names

--- app.rb
+++ app.rb
@@ -10,9 +10,9 @@ def generate_all_domains(all_domains_ordered, domain)
 	PathwayLogic.find_possible_domains(domain, all_grades)
 end
 
-def build_pathway(student_data_hash, all_domains_ordered)
+def build_pathway(student_data, all_domains_ordered)
 	final_pathways = Array.new
-	student_data_hash.each do |student, test_scores|
+	student_data.each do |student, test_scores|
 		entire_student_pathway = Array.new
 		test_scores.each do |domain|
 			entire_student_pathway << generate_all_domains(all_domains_ordered, domain)
@@ -22,20 +22,20 @@ def build_pathway(student_data_hash, all_domains_ordered)
 	final_pathways
 end
 
-def create_pathways(student_data_hash, all_final_pathways)
+def create_pathways(student_data, all_final_pathways)
 	all_pathways = Array.new
-	student_data_hash.keys.each.with_index do |student_name, index|
+	student_data.keys.each.with_index do |student_name, index|
 		all_pathways << Pathway.new(student_name, all_final_pathways[index])
 	end
 	all_pathways
 end
 
 def generate_learning_paths_file(test_data_file, domain_order_file, results_file)
-	test_data_hash = TestParser.parse_tests(test_data_file)
+	test_data = TestParser.parse_tests(test_data_file)
 	all_domains_ordered = DomainParser.parse_domains(domain_order_file)
 
-	generated_pathways = build_pathway(test_data_hash, all_domains_ordered)
-	pathways = create_pathways(test_data_hash, generated_pathways)
+	generated_pathways = build_pathway(test_data, all_domains_ordered)
+	pathways = create_pathways(test_data, generated_pathways)
 	PathwayWriter.write_to_csv(results_file, pathways)
 end
 
