d88fd7defc34fb67e24a1d75fd7fb592993d2aa2
Ashley Yang <ashley.minghui@gmail.com>
2017-03-14 10:02:26 -0500

    Add all_grades_for_domain array which is an array of grades as strings for a particular domain, add all_espark_grades which is an array of all possible espark grades as strings. Use the index of grades in all_espark_grades for comparison instead of calling .to_i on grades

--- pathway_logic.rb
+++ pathway_logic.rb
@@ -1,20 +1,19 @@
 module PathwayLogic
 	def self.find_possible_domains(domain, domain_array)
-		all_possible_grades = ["K", "1", "2", "3", "4", "5", "6"]
+		all_grades_for_domain = domain_array.map {|grade_level| grade_level[0]}
+		all_espark_grades = ["K", "1", "2", "3", "4", "5", "6"]
 
 		if !domain_array.include?(domain)		
 			grade = domain.split(".")[0]
-			domain = domain.split(".")[-1]
-			last_grade_in_domain = all_possible_grades.last
-			if all_possible_grades.index(grade) <= all_possible_grades.index(last_grade_in_domain)
-				domain_array
-			end
+			last_grade_in_domain = all_grades_for_domain.last
+			return domain_array if all_espark_grades.index(grade) < all_espark_grades.index(last_grade_in_domain)
 		else
 			domain_array[domain_array.index(domain)..-1]
 		end
 	end 	
 
 	def self.produce_pathway(ordered_domains_array, all_domains_for_pathway)
-		(ordered_domains_array & all_domains_for_pathway).first(5) 
+		(ordered_domains_array & all_domains_for_pathway).first(5)
 	end
 end
+
